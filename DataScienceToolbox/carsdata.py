f90916f41b2724589031665c4c3ab8e214a0a8be
dajofischer <David@DJF-MBP.local>
2018-11-09 15:34:12 +0100

    carsdata script

--- DataScienceToolbox/carsdata.py
+++ DataScienceToolbox/carsdata.py
@@ -1,2 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib as mp
+
+cars=pd.read_csv('/Volumes/Data/Dropbox/Python3/git/datacamp/DataScienceToolbox/cars.csv',index_col=0)
+
+print(cars)
