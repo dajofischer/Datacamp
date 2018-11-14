53a9de67392ec718219ba50a4ed003861d7affff
dajofischer <dajofischer@gmail.com>
2018-11-14 11:05:20 +0100

    Shiny FirstPeek

--- Shiny/FirstPeek.R
+++ Shiny/FirstPeek.R
@@ -1,34 +1,44 @@
-# Generate some sample data, then compute mean and standard deviation
-# in each group
-df <- data.frame(
-  gp = factor(rep(letters[1:3], each = 10)),
-  y = rnorm(30)
-)
-ds <- plyr::ddply(df, "gp", plyr::summarise, mean = mean(y), sd = sd(y))
+library(shiny)
+library(ggplot2)
+load(url("http://s3.amazonaws.com/assets.datacamp.com/production/course_4850/datasets/movies.Rdata"))
 
-# The summary data frame ds is used to plot larger red points on top
-# of the raw data. Note that we don't need to supply `data` or `mapping`
-# in each layer because the defaults from ggplot() are used.
-ggplot(df, aes(gp, y)) +
-  geom_point() +
-  geom_point(data = ds, aes(y = mean), colour = 'red', size = 3)
-
-# Same plot as above, declaring only the data frame in ggplot().
-# Note how the x and y aesthetics must now be declared in
-# each geom_point() layer.
-ggplot(df) +
-  geom_point(aes(gp, y)) +
-  geom_point(data = ds, aes(gp, mean), colour = 'red', size = 3)
-
-# Alternatively we can fully specify the plot in each layer. This
-# is not useful here, but can be more clear when working with complex
-# mult-dataset graphics
-ggplot() +
-  geom_point(data = df, aes(gp, y)) +
-  geom_point(data = ds, aes(gp, mean), colour = 'red', size = 3) +
-  geom_errorbar(
-    data = ds,
-    aes(gp, mean, ymin = mean - sd, ymax = mean + sd),
-    colour = 'red',
-    width = 0.4
+# Define UI for application that plots features of movies 
+ui <- fluidPage(
+  
+  # Sidebar layout with a input and output definitions 
+  sidebarLayout(
+    
+    # Inputs
+    sidebarPanel(
+      
+      # Select variable for y-axis
+      selectInput(inputId = "y", 
+                  label = "Y-axis:",
+                  choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"), 
+                  selected = "audience_score"),
+      # Select variable for x-axis
+      selectInput(inputId = "x", 
+                  label = "X-axis:",
+                  choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"), 
+                  selected = "critics_score")
+    ),
+    
+    # Outputs
+    mainPanel(
+      plotOutput(outputId = "scatterplot")
+    )
   )
+)
+
+# Define server function required to create the scatterplot
+server <- function(input, output) {
+  
+  # Create scatterplot object the plotOutput function is expecting
+  output$scatterplot <- renderPlot({
+    ggplot(data = movies, aes_string(x = input$x, y = input$y)) +
+      geom_point()
+  })
+}
+
+# Create a Shiny app object
+shinyApp(ui = ui, server = server)
