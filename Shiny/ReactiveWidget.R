3c7d0b1cdd359e0b43b0b7ae797a4042e0542a97
dajofischer <dajofischer@gmail.com>
2018-11-14 16:28:45 +0100

    Shiny Lecture 1 and 2

--- Shiny/ReactiveWidget.R
+++ Shiny/ReactiveWidget.R
@@ -2,28 +2,34 @@ library(shiny)
 library(ggplot2)
 load(url("http://s3.amazonaws.com/assets.datacamp.com/production/course_4850/datasets/movies.Rdata"))
 
-
 # Define UI for application that plots features of movies
 ui <- fluidPage(
-
+  
   # Sidebar layout with a input and output definitions
   sidebarLayout(
-
+    
     # Inputs
     sidebarPanel(
-
+      
       # Select variable for y-axis
-      selectInput(inputId = "y",
+      selectInput(inputId = "y", 
                   label = "Y-axis:",
-                  choices = c("runtime", "imdb_num_votes", "critics_score", "audience_score"),
-                  selected = "runtime"),
+                  choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"), 
+                  selected = "audience_score"),
+      
       # Select variable for x-axis
-      selectInput(inputId = "x",
+      selectInput(inputId = "x", 
                   label = "X-axis:",
-                  choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"),
-                  selected = "critics_score")
+                  choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"), 
+                  selected = "critics_score"),
+      
+      # Set alpha level
+      sliderInput(inputId = "alpha", 
+                  label = "Alpha:", 
+                  min = 0, max = 1, 
+                  value = 0.5)
     ),
-
+    
     # Outputs
     mainPanel(
       plotOutput(outputId = "scatterplot")
@@ -33,13 +39,13 @@ ui <- fluidPage(
 
 # Define server function required to create the scatterplot
 server <- function(input, output) {
-
+  
   # Create scatterplot object the plotOutput function is expecting
   output$scatterplot <- renderPlot({
     ggplot(data = movies, aes_string(x = input$x, y = input$y)) +
-      geom_point()
+      geom_point(alpha = input$alpha)
   })
 }
 
-# Create a Shiny app object
+# Create the Shiny app object
 shinyApp(ui = ui, server = server)
---
+++ Shiny/req.R
@@ -0,0 +1,53 @@
+library(shiny)
+library(dplyr)
+library(DT)
+
+load(url("http://s3.amazonaws.com/assets.datacamp.com/production/course_4850/datasets/movies.Rdata"))
+n_total <- nrow(movies)
+
+# Define UI for application that plots features of movies
+ui <- fluidPage(
+  
+  # Sidebar layout with a input and output definitions
+  sidebarLayout(
+    
+    # Inputs
+    sidebarPanel(
+      
+      # Text instructions
+      HTML(paste("Enter a value between 1 and", n_total)),
+      
+      # Numeric input for sample size
+      numericInput(inputId = "n",
+                   label = "Sample size:",
+                   value = 30,
+                   min = 1, max = n_total,
+                   step = 1)
+      
+    ),
+    
+    # Output: Show data table
+    mainPanel(
+      DT::dataTableOutput(outputId = "moviestable")
+    )
+  )
+)
+
+# Define server function required to create the scatterplot
+server <- function(input, output) {
+  
+  # Create data table
+  output$moviestable <- DT::renderDataTable({
+    req(input$n)
+    movies_sample <- movies %>%
+      sample_n(input$n) %>%
+      select(title:studio)
+    DT::datatable(data = movies_sample, 
+                  options = list(pageLength = 10), 
+                  rownames = FALSE)
+  })
+  
+}
+
+# Create a Shiny app object
+shinyApp(ui = ui, server = server)
