12325a703fe316dd40ce30f3fcbd16733ac07ee4
dajofischer <dajofischer@gmail.com>
2018-11-27 21:49:24 +0100

    DataScienceToolbox 2 final

---
+++ DataScienceToolbox2/E20 Dictionaries for data science.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+cwd = os.getcwd()
+df=cwd+'/DataScienceToolbox2/world_ind_pop_data.csv'
+df=pd.read_csv(df)
+
+feature_names=['CountryName',
+ 'CountryCode',
+ 'IndicatorName',
+ 'IndicatorCode',
+ 'Year',
+ 'Value']
+
+row_vals=['Arab World',
+ 'ARB',
+ 'Adolescent fertility rate (births per 1,000 women ages 15-19)',
+ 'SP.ADO.TFRT',
+ '1960',
+ '133.56090740552298']
+
+# Zip lists: zipped_lists
+zipped_lists = zip(feature_names,row_vals)
+
+# Create a dictionary: rs_dict
+rs_dict = dict(zipped_lists)
+
+# Print the dictionary
+print(rs_dict)
---
+++ DataScienceToolbox2/E24 Processing data in chunks (1).py
@@ -0,0 +1,31 @@
+import os
+cwd = os.getcwd()
+cwd=cwd+'/DataScienceToolbox2/world_ind_pop_data.csv'
+# Open a connection to the file
+with open(cwd) as file:
+
+    # Skip the column names
+    file.readline()
+
+    # Initialize an empty dictionary: counts_dict
+    counts_dict = {}
+
+    # Process only the first 1000 rows
+    for j in range(1000):
+
+        # Split the current line into a list: line
+        line = file.readline().split(',')
+
+        # Get the value for the first column: first_col
+        first_col = line[0]
+
+        # If the column value is in the dict, increment its value
+        if first_col in counts_dict.keys():
+            counts_dict[first_col] += 1
+
+        # Else, add to the dict and set value to 1
+        else:
+            counts_dict[first_col] = 1
+
+# Print the resulting dictionary
+print(counts_dict)
