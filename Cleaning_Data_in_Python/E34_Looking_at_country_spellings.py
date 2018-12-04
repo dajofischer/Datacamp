f105b12ee9b0000691b553d3b2c47c71f0a2adad
dajofischer <dajofischer@gmail.com>
2018-12-04 20:01:25 +0100

    Cleaning course

--- Cleaning_Data_in_Python/E34_Looking_at_country_spellings.py
+++ Cleaning_Data_in_Python/E34_Looking_at_country_spellings.py
@@ -1,20 +1,20 @@
 # Create the series of countries: countries
-countries = ____
+countries = gapminder['country']
 
 # Drop all the duplicates from countries
-countries = ____
+countries = countries.drop_duplicates()
 
 # Write the regular expression: pattern
-pattern = '^[____]*$'
+pattern = '^[A-Za-z\.\s]*$'
 
 # Create the Boolean vector: mask
-mask = countries.str.contains(____)
+mask = countries.str.contains(pattern)
 
 # Invert the mask: mask_inverse
-mask_inverse = ____
+mask_inverse = ~mask
 
 # Subset countries using mask_inverse: invalid_countries
-invalid_countries = ____
+invalid_countries = countries.loc[mask_inverse]
 
 # Print invalid_countries
 print(invalid_countries)
