640567d7caae6e8fdfaf2380a3d25d9239d06d8f
dajofischer <dajofischer@gmail.com>
2018-12-27 16:09:04 +0100

    Database in python

---
+++ DataBase_in_Python_Intro/E09_Filter_data_selected_from_a_Table__Expressions.py
@@ -0,0 +1,9 @@
+# Create a query for the census table: stmt
+stmt = select([census])
+
+# Append a where clause to match all the states in_ the list states
+stmt = stmt.where(census.columns.state.in_(states))
+
+# Loop over the ResultProxy and print the state and its population in 2000
+for x in connection.execute(stmt):
+    print(x['state'], x['pop2000'])
